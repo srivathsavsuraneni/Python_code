2e588ed7f407c0d55f747c84b5330412aeee599c
srivathsavsuraneni <srivathsavsuraneni4@gmail.com>
2026-06-09 12:28:34 -0500

    my commit

---
+++ function1.py
@@ -0,0 +1,9 @@
+class MyClass:
+    def validate_age(self):
+        age = int(input("Enter your age: "))
+        if age < 18:
+            print("You are a minor.")
+        elif age >= 18 and age < 60:
+            print("You are an adult.")
+        else:
+            print("You are a senior citizen.")
