b9c3e2f8149a95a406f6efee962d367552b0c9ed
srivathsavsuraneni <srivathsavsuraneni4@gmail.com>
2026-06-09 12:22:57 -0500

    fourth commit

--- third.py
+++ third.py
@@ -1,1 +1,10 @@
-print("bye bye")
+sum = []
+def main(a,b):
+    print (a, " ", b, end = ' ')
+
+    for i in range(10):
+        sum.append(a+b)
+        a, b = b, sum[i]
+        print (sum[i], end = ' ')
+
+main(0, 1)
